5deff123f0cdbcf515c135f54441115e5b3960e5
kosz <coszmin@gmail.com>
2016-07-14 19:09:40 -0400

    issues with this in javascript

--- observable.js
+++ observable.js
@@ -50,6 +50,19 @@ class Controller {
       // execution context, then the `this` referece inside the passed function
       // will be lost, unless if we specifically bind the function's `this`
       // to a certain object, in this case `this`
+
+      // javascript provides 3 ( now 4 ) methods to get around this
+      // the .bind function
+      // the .call function
+      // the .apply function
+      // and now-a-days the arrow function
+      // an arrow function will ALWAYS keep it's `this` bound to it's oringinal
+      // context
+
+      // in ES/TS it is a good idea, to not use instance methods EVER
+      // instead, write them as instance properties, containing arrow functions
+      // this way, if you pass the function to another function to be called
+      // the function does not loose it's reference to `this`
   }
 
   // when defined as a function ( non arrow function )
@@ -59,7 +72,7 @@ class Controller {
   // unless, if you use .bind(this), on the function before passing it
 
   // keep in mind that .bind(this) will actually return a NEW function
-  observer (value) {
+  observer = (value) => {
     console.log(`${this.hello} ${value}`);
   }
 
